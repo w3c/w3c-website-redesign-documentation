270afd3cc0ecb332c85a862c6544c42efe8e39d7
Alan Isaacson <alan@studio24.net>
2020-05-20 09:40:54 +0100

    Initial commit for deployemnt config

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # w3c-website-redesign-documentation
-Repo for project documention for the 2020 W3C website redesign project 
+
+Documentation site hosted on the Studio24 web server in AWS Singlecloud 1
+
+## Getting started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See [deployment](#markdown-header-deployment) for notes on how to deploy the project on staging and live environments.
+
+## Site URLs
+
+* Live - https://w3c.studio24.net/
+* Staging - http://w3c.studio24.dev/
+
+The live site is cached via Cloudflare
+
+## Installation
+This will clone the repo into a new folder with the same name as the repo.
+
+````bash
+cd ~/Sites
+git clone git@github.com:studio24/w3c-website-redesign-documentation.git
+```
+
+## Deployment
+
+We use [Deployer](https://deployer.org) for deployment.  
+__Note__ Currently there are no shared files/directories or apache writable configuration
+
+Deploy to production (deploys master branch):
+
+```
+dep deploy production
+```
+
+Deploy to staging:
+
+```
+dep deploy staging
+```
+
+Deploy a custom branch to staging:
+
+```
+dep deploy staging --branch=branch-name-to-deploy
+``` 
+
+
+## Authors
+- **Author Name** - *Alan Isaacson*
---
+++ deploy.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'W3C Document Portal');
+
+// Project repository
+set('repository', 'git@github.com:studio24/w3c-website-redesign-documentation');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+set('shared_files', ['config/wp-config.local.php']);
+set('shared_dirs', ['web/wp-content/uploads','.well-known']);
+
+// Writable dirs by web server
+set('writable_dirs', ['web/wp-content/uploads']);
+set('allow_anonymous_stats', false);
+
+// Custom
+set('keep_releases', 10);
+
+
+// Hosts
+
+host('production')
+    ->hostname('63.34.69.8')
+    ->user('deploy')
+    ->set('deploy_path','/data/var/www/vhosts/w3c/production');
+
+host('staging')
+    ->stage('staging')
+    ->hostname('63.34.69.8')
+    ->user('deploy')
+    ->set('deploy_path','/data/var/www/vhosts/w3c/staging');
+
+
+
+
+// Tasks
+
+desc('Deploy W3C Document Portal');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+//    'deploy:composer:s3',
+//    'deploy:composer:theme',
+//    'deploy:shared',
+//    'deploy:writable',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
+
+//task('deploy:composer:s3',function(){
+
+//    cd('{{release_path}}/web/content/plugins/S3-Uploads');
+//    run('/usr/local/bin/composer install  --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader');
+
+//});
+
+//task('deploy:composer:theme',function(){
+
+//    cd('{{release_path}}/web/content/themes/equaliteas/');
+//    run('/usr/local/bin/composer install  --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader');
+
+//});
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+
